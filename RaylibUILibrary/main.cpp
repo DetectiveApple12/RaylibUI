47cebb174db34915f9e2317ae27342fe81458487
Yair Shafran <yairshafran1@gmail.com>
2024-08-14 02:28:51 +0300

    Added to Example

--- RaylibUILibrary/main.cpp
+++ RaylibUILibrary/main.cpp
@@ -10,7 +10,7 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Showcase");
 
     InputBar name (500, 200, 400, 50, { 255, 255, 255, 255 }, { 0, 0, 0, 255 }, 20, "Username...", {200, 200, 200, 255}, 1, 5);
-    Button button (500, 400, "Click", [](void* x) {std::cout << (char*)(x) << std::endl;}, (void*)"Hello World!");
+    Button button (500, 400, "Click", [](void* x) {std::cout << ((InputBar*)x)->GetText() << std::endl;}, (void*)&name);
 
     SetTargetFPS(60);
 
